555ee9c4a8cb687cd0c255e9b1ea2c0aafb2161e
Hexyl <hayuki0319@gmail.com>
2024-03-22 11:10:03 +0900

    Page Update
    ・ヘッダを導入し、ヘッダ部分にsection linkを導入
    ・フッダを挿入

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,31 @@
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
+const sections = [
+  { title: 'Home', url: 'https://hexylab.github.io/playground/' },
+];
+
 function App() {
   return (
     <Box display="flex" flexDirection="column" minHeight="100vh">
-      <Header />
-      <img src={`${process.env.PUBLIC_URL}/images/header-image.jpg`} alt="Header" />
-      <Box mt={4} mb={4}> {/* ここでBoxを使ってマージンを追加 */}
-        <Content />
-      </Box>
+      <Header title="Hexyl's Playground" sections={sections} />
+      <Container maxWidth="lg"> {/* 最大幅をlgに設定 */}
+        <Box
+          component="img"
+          sx={{
+            width: '100%', // 画像を画面幅いっぱいに広げる
+            height: 200, // 画像の高さを指定
+            objectFit: 'cover', // 画像のサイズを調整してBoxにフィットさせる
+          }}
+          src={`${process.env.PUBLIC_URL}/images/header-image.jpg`}
+          alt="ページのバナー"
+        />
+        <Box mt={4} mb={4}> {/* ここでBoxを使ってマージンを追加 */}
+          <Content />
+        </Box>
+      </Container>
       <Footer />
     </Box>
   );
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,13 +1,78 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+import Link from '@mui/material/Link';
 
-const Header = () => (
-  <AppBar position="static" style={{ backgroundImage: 'url(背景画像のURL)', backgroundSize: 'cover' }}>
-    <Toolbar>
-      <Typography variant="h4">
-        Home
+const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          height: 40,
+          backgroundColor: 'gray',
+        },
+      },
+    },
+    MuiToolbar: {
+      styleOverrides: {
+        root: {          
+          minHeight: 40, // Toolbarの高さを40pxに設定
+          paddingTop: '0px', // 上のパディングを調整
+          paddingBottom: '0px', // 下のパディングを調整
+        },
+      },
+    },
+  },
+});
+
+
+function Header(props) {
+  const { sections, title } = props;
+
+  return(
+  <ThemeProvider theme={theme}>
+  <AppBar position="fixed">
+    <Toolbar
+      component="nav"
+      variant="dense"
+      sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+    >
+      <Typography
+          component="h2"
+          variant="h6"
+          color="inherit"
+          align="flex-start"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+        {title}
       </Typography>
+      {sections.map((section) => (
+        <Link
+          color="inherit"
+          noWrap
+          key={section.title}
+          variant="body2"
+          href={section.url}
+          sx={{ p: 1, flexShrink: 0 }}
+        >
+          {section.title}
+        </Link>
+      ))}
     </Toolbar>
   </AppBar>
-);
+  </ThemeProvider>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
